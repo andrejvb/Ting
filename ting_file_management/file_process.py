91f32a6c1e9b4cb371df29f952e15e42e3e6d074
andrejvb <andre.jvb@gmail.com>
2023-07-20 13:49:17 -0300

    third, fourth and fifth requirement

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,37 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+
+    file_dict = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(lines),
+        'linhas_do_arquivo': lines,
+    }
+
+    try:
+        instance.search(instance.items.index(file_dict))
+        print(f"Arquivo '{path_file}' já foi processado anteriormente.\n")
+        return None
+    except ValueError:
+        instance.enqueue(file_dict)
+        print(file_dict)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance.items) == 0:
+        print('Não há elementos')
+        return
+
+    removed_file = instance.dequeue()
+    print(f'Arquivo {removed_file["nome_do_arquivo"]} removido com sucesso')
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        metadata = instance.search(position)
+        print(metadata)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,20 +3,20 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self.__items = []
+        self.items = []
 
     def __len__(self):
-        return len(self.__items)
+        return len(self.items)
 
     def enqueue(self, value):
-        self.__items.append(value)
+        self.items.append(value)
 
     def dequeue(self):
-        if (self.__items) == 0:
+        if (self.items) == 0:
             return None
-        return self.__items.pop(0)
-    
+        return self.items.pop(0)
+
     def search(self, index):
-        if index < 0 or index >= len(self.__items):
+        if index < 0 or index >= len(self.items):
             raise IndexError('Índice Inválido ou Inexistente')
-        return self.__items[index]
+        return self.items[index]
